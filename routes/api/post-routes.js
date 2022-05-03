7c798a57e5a309302633b7da6d403e57e6463677
Brian7950 <brianh7950@gmail.com>
2022-05-02 23:20:17 -0600

    added comments and refactored other routes to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 //get all users
@@ -7,14 +7,30 @@ router.get('/', (req, res) => {
     console.log('===================');
     Post.findAll({
         //query
-        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
 
         //think of include as join property
+        //include the Comment model here:
         include: [
             {
-                model: User,
-                attributes: ['username']
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include:{
+                    model: User,
+                    attributes:['username']
+                }
+            },
+
+            {
+                model:User,
+                attributes:['username']
             }
         ]
     })
@@ -36,8 +52,17 @@ router.get('/:id', (req, res) => {
         ],
         include: [
             {
-                model: User,
-                attributes: ['username']
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include:{
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+
+            {
+                model:User,
+                attributes:['username']
             }
         ]
     })
@@ -71,12 +96,12 @@ router.post('/', (req, res) => {
 //PUT create the vote
 router.put('/upvote', (req, res) => {
     //custom static method created in models/Post.js
-    Post.upvote(req.body, {Vote})
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 //update a post Title
